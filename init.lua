33c86a6ad4c64ebc80f11ab62b3ae6de716f96f1
priestleydj <priestleydj@gmail.com>
2025-01-30 00:59:29 +0000

    Fix typescript and javascript indentation and disable listchars

--- init.lua
+++ init.lua
@@ -145,7 +145,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
@@ -688,7 +688,15 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {
+          settings = {
+            javascript = {
+              format = {
+                indentSize = 2,
+              },
+            },
+          },
+        },
         --
 
         lua_ls = {
