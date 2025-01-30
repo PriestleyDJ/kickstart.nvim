8328c43fdd882c85de1b95da83b9093e431c801e
priestleydj <priestleydj@gmail.com>
2025-01-30 00:04:22 +0000

    add ts and js lsp

--- init.lua
+++ init.lua
@@ -676,10 +676,7 @@ require('lazy').setup({
               disableOrganizeImports = true,
             },
             python = {
-              analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
-              },
+              analysis = {},
             },
           },
         },
